72b3ea9e4e4515083063d71890c7660ea3d7ab68
KyoheiNakafuji <kyohei.nakafuji@freamap.co.jp>
2018-11-12 23:45:01 +0900

    nuxt-sass-resources-loader導入

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,6 +42,7 @@ module.exports = {
   modules: [
     // Doc: https://github.com/nuxt-community/axios-module#usage
     '@nuxtjs/axios',
+    ['nuxt-sass-resources-loader', '@/assets/sass/variable.scss'],
   ],
   /*
   ** Axios module configuration
