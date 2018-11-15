73fec96568cf5c87aacdaac69a74eb795f32e1bb
KyoheiNakafuji <kyohei.nakafuji@freamap.co.jp>
2018-11-15 11:24:23 +0900

    開発実績詳細ページ作成

---
+++ pages/works/detail.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    works detail page
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  layout: 'sub',
+  asyncData(context) {
+    context.store.dispatch('setPage', 'works')
+  }
+}
+</script>
+
+<style>
+</style>
