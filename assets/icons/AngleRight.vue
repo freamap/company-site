92ff84d2aa2811a303986192ad2865b8b7c8051b
KyoheiNakafuji <kyohei.nakafuji@freamap.co.jp>
2018-11-19 22:59:01 +0900

    iconsを作成

---
+++ assets/icons/AngleRight.vue
@@ -0,0 +1,27 @@
+<template>
+  <svg
+    id="angle-right"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 19 19"
+  >
+    <defs>
+      <style>
+        .cls-1 {
+        fill: none;
+        }
+      </style>
+    </defs>
+    <path
+      id="パス_50"
+      data-name="パス 50"
+      d="M8.59,14.384l3.626-3.634L8.59,7.116,9.706,6l4.75,4.75L9.706,15.5Z"
+      transform="translate(-1.79 -1.25)"
+    />
+    <path
+      id="パス_51"
+      data-name="パス 51"
+      class="cls-1"
+      d="M0,0H19V19H0Z"
+    />
+  </svg>
+</template>
