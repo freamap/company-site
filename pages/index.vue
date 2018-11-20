c8b272547db5f9c2ea8143cd2e2dd7c96e7c2c4f
KyoheiNakafuji <kyohei.nakafuji@freamap.co.jp>
2018-11-20 11:36:00 +0900

    newsをvuexで定義

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,45 @@
 <template>
-  <div>
-    top
+  <div class="top container">
+    <div class="content">
+      <div>
+        ニュース
+      </div>
+      <div
+        v-for="detail in news"
+        :key="detail.id"
+      >
+        <div>
+          {{ detail.update }}
+        </div>
+        <div>
+          {{ detail.description }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapState, mapActions } from 'vuex'
+
 export default {
   asyncData(context) {
     context.store.dispatch('setPage', context.store.state.pages.pages.top.url)
+  },
+  computed: {
+    ...mapState('news', ['news'])
   }
 }
 </script>
 
-<style>
+<style scoped lang="scss">
+.top {
+  margin-top: -50px;
+
+  > .content {
+    padding-top: 90px;
+    padding-bottom: 120px;
+    background: #ffffff;
+  }
+}
 </style>
