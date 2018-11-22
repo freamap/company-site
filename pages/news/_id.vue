b06fc02e208e7e1f8538d5f62242795b9738af5c
KyoheiNakafuji <kyohei.nakafuji@freamap.co.jp>
2018-11-22 16:31:48 +0900

    ニュース詳細ページ実装

--- pages/news/_id.vue
+++ pages/news/_id.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>
+  <div class="news-detail">
+    <div class="update">
+      {{ news.update }}
+    </div>
+    <div class="contents">
+      {{ news.contents }}
+    </div>
     news detail {{ $route.params.id }}
   </div>
 </template>
@@ -12,9 +18,21 @@ export default {
       'setPage',
       context.store.state.pages.pages.news.details[context.params.id].url
     )
+
+    return {
+      news: context.store.state.news.news[context.params.id]
+    }
   }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.news-detail {
+  padding: 90px 140px 120px 140px;
+  display: flex;
+
+  .update {
+    margin-right: 160px;
+  }
+}
 </style>
