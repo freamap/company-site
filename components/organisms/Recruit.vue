4a5bdc028c40454b47715627bc9118bd7674ec57
YurikoYatoh <yuriko.yatoh@freamap.co.jp>
2018-11-29 17:12:37 +0900

    Recruit.vue recruit.jsの作成、適用（レイアウト調整中、indexのリンクは仮）

---
+++ components/organisms/Recruit.vue
@@ -0,0 +1,99 @@
+<template>
+  <div>
+    <div
+      v-for="detail in recruit"
+      :key="detail.id"
+      class="content"
+    >
+      <div class="occupation">
+        {{ detail.occupation }}
+      </div>
+      <div class="workplace">
+        {{ detail.workplace }}
+      </div>
+      <div class="fee">
+        {{ detail.fee }}
+      </div>
+      <div class="detail-button">
+        <nuxt-link
+          :to="{path: '/news/' + detail.id}"
+          tag="div"
+        >
+          <ArrowDown />
+        </nuxt-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+import ArrowDown from '~/assets/icons/ArrowDown.vue'
+
+export default {
+  components: {
+    ArrowDown
+  },
+  props: {
+    length: {
+      type: Number,
+      default: Number.MAX_VALUE,
+      required: false
+    }
+  },
+  computed: {
+    ...mapState('recruit', ['recruit'])
+  },
+  methods: {
+    ...mapActions(['changePage'])
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.content {
+  display: flex;
+  align-items: center;
+  box-sizing: content-box;
+  font-size: 15px;
+
+  .occupation {
+    margin-right: 200px;
+  }
+
+  .workplace {
+    margin-right: 200px;
+  }
+
+  .fee {
+    margin-right: 170px;
+  }
+
+  .detail-button {
+    flex-basis: 38px;
+    height: 38px;
+
+    div {
+      width: 100%;
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      cursor: pointer;
+      outline: 0;
+      border: none;
+      padding: 0;
+
+      svg {
+        transform: rotate(-90deg);
+        width: 20px;
+        fill: currentColor;
+      }
+    }
+  }
+
+  &:last-child {
+    border-bottom: 1px solid #e8e9ea;
+  }
+}
+</style>
--- pages/recruit/index.vue
+++ pages/recruit/index.vue
@@ -1,10 +1,32 @@
 <template>
-  <div>
-    recruit page
+  <div class="recruit-page">
+    <div class="tytle">
+      <div class="bold">
+        募集職種一覧
+      </div>
+      <div class="date">
+        最終更新日: 2018/06/16
+      </div>
+    </div>
+    <div class="list">
+      <div>
+        職種
+      </div>
+      <div>
+        勤務地
+      </div>
+      <div>
+        報酬
+      </div>
+    </div>  
+    <div>
+      <Recruit />
+    </div>
   </div>
 </template>
 
 <script>
+import Recruit from '~/components/organisms/Recruit.vue'
 import { mapActions } from 'vuex'
 
 export default {
@@ -20,9 +42,54 @@ export default {
       url: context.route.fullPath,
       topicPath: topicPath
     })
+  },
+  components: {
+    Recruit
   }
 }
 </script>
 
-<style>
+<style scoped lang="scss">
+.recruit-page {
+  font-family: Noto Sans CJK JP;
+  margin-left: 140px;
+  margin-right: 140px;
+  display: flex;
+  flex-direction: column;
+
+  .tytle {
+    display: flex;
+    margin-top: 55px;
+    margin-bottom: 50px;
+    width: 100%;
+
+    .bold {
+      font-size: 18px;
+      color: #000000;
+      font-weight: bold;
+      text-align: left;
+    }
+
+    .date {
+      font-size: 13px;
+      color: #767676;
+      text-align: right;
+    }
+  }
+
+  .list {
+    display: flex;
+    color: #191919;
+    font-size: 13px;
+    background-color: #fafafa;
+    height: 50px;
+    width: 100%;
+    margin-bottom: 31px;
+
+    > div {
+      margin-top: 16px;
+      margin-bottom: 15px;
+    }
+  }
+}
 </style>
