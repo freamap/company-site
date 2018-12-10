2e04119383f0331f81ad9c0dc1240cebf4dda115
KyoheiNakafuji <kyohei.nakafuji@freamap.co.jp>
2018-12-10 12:17:31 +0900

    configファイル作成

--- server/api/news.js
+++ server/api/news.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
-
-const news = []
+var config = require('config');
 
 router.get('/', function(req, res, next) {
+  let news = [
+    process.env.DB_URL,
+    process.env.NODE_ENV,
+    config.db.test
+  ]
+
   res.json(news)
 });
 
