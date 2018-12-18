5b2daebe419a3ad10b4e06a07ae0ba5487f90d9b
KyoheiNakafuji <kyohei.nakafuji@freamap.co.jp>
2018-12-18 14:53:56 +0900

    ニュース一覧取得のAPIを実装

--- server/api/news.js
+++ server/api/news.js
@@ -1,15 +1,28 @@
 var express = require('express');
 var router = express.Router();
-var config = require('config');
+var AWS = require('aws-sdk');
+AWS.config.update({
+  region: "ap-northeast-1",
+  endpoint: process.env.DB_URL || 'http://localhost:8000',
+  accessKeyId: process.env.ACCESS_KEY_ID || "123456789",
+  secretAccessKey: process.env.SECRET_ACCESS_KEY_ID || ""
+});
+var docClient = new AWS.DynamoDB.DocumentClient();
 
 router.get('/', function(req, res, next) {
-  let news = [
-    process.env.DB_URL,
-    process.env.NODE_ENV,
-    config.db.test
-  ]
+  var table = "news";
 
-  res.json(news)
+  var params = {
+      TableName: table,
+  };
+
+  docClient.scan(params, function(err, data) {
+    if (err) {
+      res.json(console.log(err))
+    } else {
+      res.json(data.Items)
+    }
+  });
 });
 
 module.exports = router;
