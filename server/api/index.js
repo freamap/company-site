212526f5eab5513fa8ae39e6ad27ac320cb68197
KyoheiNakafuji <kyohei.nakafuji@freamap.co.jp>
2018-12-21 02:11:11 +0900

    404を仮実装

--- server/api/index.js
+++ server/api/index.js
@@ -5,6 +5,11 @@ const app = express()
 
 app.use('/news', news)
 
+app.use(function(req, res, next){
+	res.status(404);
+	res.end('API NOT FOUND : ' + req.path);
+});
+
 module.exports = {
   path: '/api',
   handler: app,
