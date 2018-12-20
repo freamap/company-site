9029ccb94ed4520ecd42a87c958909c42ca8ebc0
KyoheiNakafuji <kyohei.nakafuji@freamap.co.jp>
2018-12-20 22:06:27 +0900

    ページネーション実装

--- server/api/news.js
+++ server/api/news.js
@@ -10,19 +10,77 @@ AWS.config.update({
 var docClient = new AWS.DynamoDB.DocumentClient();
 
 router.get('/', function(req, res, next) {
-  var table = "news";
+  let create = req.query.create
+  let newsId = Number(req.query.news_id)
 
-  var params = {
-      TableName: table,
-  };
-
-  docClient.scan(params, function(err, data) {
-    if (err) {
-      res.json(console.log(err))
-    } else {
-      res.json(data.Items)
-    }
-  });
+  getNews(create, newsId).then(news => {
+    res.json(news)
+  }).catch(err => {
+    console.log(err)
+    res.json("取得に失敗しました。")    
+  })
 });
+
+async function getNews(create, newsId) {
+  let newsPickup = await scanNewsPickup()
+  let news = await scanNews(create, newsId, newsPickup)
+
+  return news
+}
+
+function scanNewsPickup() {
+  return new Promise((resolve, reject) => {
+    let params = {
+      TableName: "news_pickup"
+    };
+  
+    docClient.scan(params, function(err, data) {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(data.Items)
+      }
+    })
+  })
+}
+
+function scanNews(create, newsId, newsPickup) {
+  return new Promise((resolve, reject) => {
+    let limit = 3
+    if (!create || !newsId) {
+      limit = limit - newsPickup.length
+    }
+
+    let params = {
+      TableName: "news",
+      IndexName: "SortCreateIndex",
+      KeyConditionExpression: "#status = :statusCode",
+      ExpressionAttributeValues: {
+        ":statusCode": 1
+      },
+      ExpressionAttributeNames:{
+        "#status": "status"
+      },
+      ScanIndexForward: false,
+      Limit: limit
+    };
+
+    if (create && newsId) {
+      params["ExclusiveStartKey"] = {
+        create: create,
+        news_id: newsId,
+        status: 1
+      }
+    }
+
+    docClient.query(params, function(err, data) {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(data.Items)
+      }
+    })
+  })
+}
 
 module.exports = router;
