1729035fb19fa308c824c11c56a3f7847c90fd6a
KyoheiNakafuji <kyohei.nakafuji@freamap.co.jp>
2018-12-21 15:34:55 +0900

    worksのAPIを作成

--- server/api/index.js
+++ server/api/index.js
@@ -1,11 +1,13 @@
 const news = require('./news.js')
 const recruits = require('./recruits.js')
+const works = require('./works.js')
 const express = require('express')
 
 const app = express()
 
 app.use('/news', news)
 app.use('/recruits', recruits)
+app.use('/works', works)
 
 app.use(function(req, res, next){
 	res.status(404);
