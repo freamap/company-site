ce81b8618d5ea96a46eaba083d697b8642fd0d5e
KyoheiNakafuji <kyohei.nakafuji@freamap.co.jp>
2018-12-21 12:26:23 +0900

    error時のログ出力を修正

--- server/common/common.js
+++ server/common/common.js
@@ -1,5 +1,6 @@
 module.exports = {
-  createApiErrorResponse: (errorCode, message) => {
+  apiErrorResponse: (apiName, err, errorCode, message) => {
+    console.log("API:" + apiName + "\nERR:" + err + "\nERROR_CODE:" + errorCode + "\nmessage:" + message)
     return {
       errorCode: errorCode,
       message: message
