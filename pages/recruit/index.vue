c34e80f4ca69a8cd3f354d466682281ac64294d7
KyoheiNakafuji <kyohei.nakafuji@freamap.co.jp>
2018-12-30 14:41:10 +0900

    採用ページレイアウト修正

--- pages/recruit/index.vue
+++ pages/recruit/index.vue
@@ -6,6 +6,7 @@
           募集職種一覧
         </h2>
         <div class="update">
+          最終更新日:
           <time
             :datetime="latestUpdate | formatDateTimeTag"
           >
@@ -73,26 +74,55 @@ export default {
 <style scoped lang="scss">
 .recruit-page {
   font-family: Noto Sans CJK JP;
-  margin: 90px 140px 120px 140px;
+  padding: 25px 20px 70px 20px;
   display: flex;
   flex-direction: column;
 
+  @include mq(md) {
+    padding: 90px 140px 120px 140px;
+  }
+
   .firstLine {
     display: flex;
-    margin-top: 55px;
-    margin-bottom: 50px;
+    margin-top: 0px;
+    margin-bottom: 25px;
     width: 100%;
+    flex-direction: column;
+
+    @include mq(md) {
+      margin-top: 55px;
+      margin-bottom: 50px;
+      align-items: center;
+      justify-content: space-between;
+      flex-direction: row;
+    }
 
     .title {
       font-size: 1.8rem;
       color: #000000;
       font-weight: bold;
+      text-align: center;
+
+      @include mq(md) {
+        text-align: left;
+      }
     }
 
     .update {
       font-size: 1.3rem;
       color: #767676;
-      margin-left: auto;
+      text-align: center;
+      margin-top: 10px;
+
+      @include mq(md) {
+        text-align: left;
+        margin-top: 0px;
+      }
+
+      * {
+        font-size: inherit;
+        color: inherit;
+      }
     }
   }
 }
