e0d4ff9c742d9c231abc3b6443921b189210e15e
KyoheiNakafuji <kyohei.nakafuji@freamap.co.jp>
2019-01-02 02:12:28 +0900

    開発実績詳細初期処理実装

--- pages/works/_id.vue
+++ pages/works/_id.vue
@@ -6,26 +6,42 @@
 
 <script>
 import { mapState } from 'vuex'
+import axios from 'axios'
 
 export default {
   layout: 'sub',
   head() {
     return {
-      title: this.pages.title
+      title: this.title
     }
   },
   async fetch({ app, route, store, params }) {
+    let baseUrl = process.server
+      ? process.env.apiBaseURLLocal
+      : process.env.apiBaseURL
+    let { data } = await axios.get(baseUrl + '/api/works/' + params.id)
+    await store.dispatch('works/setCurrentWork', data)
+
+    let page = app.getPage('works')
+    let title = data.title ? data.title : '開発実績詳細'
     let topicPath = [
       {
-        url: store.state.pages.pages.works.url,
-        title: store.state.pages.pages.works.title
+        url: page.url,
+        title: page.title
+      },
+      {
+        url: route.fullPath,
+        title: title
       }
     ]
     await store.dispatch('setPage', {
       topicPath: topicPath,
       originPage: page,
-      url: route.fullPath
+      title: title
     })
+  },
+  computed: {
+    ...mapState(['title'])
   }
 }
 </script>
