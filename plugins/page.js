ab587102bcf52ef1a629b310ef9f6e385e3b2399
KyoheiNakafuji <kyohei.nakafuji@freamap.co.jp>
2019-01-12 10:37:03 +0900

    ogp処理を共通化

--- plugins/page.js
+++ plugins/page.js
@@ -1,6 +1,7 @@
 export default ({ app }, inject) => {
   app.getPage = getPage
   app.getPages = getPages
+  app.getMetaData = getMetaData
 }
 
 const getPage = function(pageName) {
@@ -9,6 +10,56 @@ const getPage = function(pageName) {
 
 const getPages = function() {
   return pages
+}
+
+const getMetaData = function(pageName) {
+  let page = pages[pageName]
+  let meta = [
+    {
+      hid: 'og:url',
+      property: 'og:url',
+      content: 'https://freamap.co.jp' + page.url
+    }
+  ]
+
+  if (page.meta.title) {
+    meta.push({
+      hid: 'og:title',
+      property: 'og:title',
+      content: page.meta.title
+    })
+  }
+
+  if (page.meta.description) {
+    meta.push({
+      hid: 'description',
+      name: 'description',
+      content: page.meta.description
+    })
+    meta.push({
+      hid: 'og:description',
+      name: 'og:description',
+      content: page.meta.description
+    })
+  }
+
+  if (page.meta.image) {
+    meta.push({
+      hid: 'og:image',
+      name: 'og:image',
+      content: page.meta.image
+    })
+  }
+
+  if (page.meta.image) {
+    meta.push({
+      hid: 'og:image',
+      name: 'og:image',
+      content: page.meta.image
+    })
+  }
+
+  return meta
 }
 
 const pages = {
@@ -27,7 +78,13 @@ const pages = {
     box: true,
     upContentsMobile: true,
     boxMobile: true,
-    globalMenu: true
+    globalMenu: true,
+    meta: {
+      title: '企業理念',
+      description: 'freamapの企業理念ページです。',
+      url: 'https://freamap.co.jp/philosophy',
+      image: 'https://freamap.co.jp/'
+    }
   },
   works: {
     url: '/works',
