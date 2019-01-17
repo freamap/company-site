60b6ce69a2a78e9df29b3d606f43dcced3189868
KyoheiNakafuji <kyohei.nakafuji@freamap.co.jp>
2019-01-17 19:54:34 +0900

    endpointを本番環境で設定しないように修正

--- server/aws/aws.js
+++ server/aws/aws.js
@@ -1,10 +1,17 @@
 var AWS = require('aws-sdk');
-AWS.config.update({
+
+let awsConfig = {
   region: "ap-northeast-1",
   endpoint: process.env.DB_URL || 'http://localhost:8000',
   accessKeyId: process.env.ACCESS_KEY_ID || "123456789",
   secretAccessKey: process.env.SECRET_ACCESS_KEY_ID || ""
-});
+}
+
+if (awsConfig.secretAccessKey === "") {
+  awsConfig = {...awsConfig, endpoint: "http://localhost:8000" }
+}
+
+AWS.config.update(awsConfig);
 
 module.exports = {
   docClient: new AWS.DynamoDB.DocumentClient()
