b277a3de21c1a5221f213594256b9c3d55ee8c01
KyoheiNakafuji <kyohei.nakafuji@freamap.co.jp>
2019-02-12 11:08:40 +0900

    プライバシーポリシーの追加

--- pages/privacy-policy/index.vue
+++ pages/privacy-policy/index.vue
@@ -30,7 +30,7 @@
     <div class="content">
       <div class="small-content">
         <p>
-          当社ホームページでは、お問い合わせフォームを設置しています。<br>
+          当社ホームページでは、<nuxt-link to="/contact">お問い合わせフォーム</nuxt-link>を設置しています。<br>
           当社ホームページ訪問者が<nuxt-link to="/contact">お問い合わせフォーム</nuxt-link>からお問い合わせを行う場合、以下の個人情報を取得します。
         </p>
         <ul>
@@ -73,7 +73,7 @@
       <h5>第三者によるCookie情報の取得について</h5>
       <div class="small-content">
         <p>
-          当ブログでは、Google株式会社をはじめとする第三者の解析ツールを利用しています。<br>
+          当社ホームページでは、Google株式会社をはじめとする第三者の解析ツールを利用しています。<br>
           これに関連して、訪問者のCookie情報などを取得して利用している場合があります。<br>
           当該第三者によって取得されたCookie情報等は、当該第三者のプライバシーポリシーに従って取り扱われます。
         </p>
@@ -86,8 +86,78 @@
       </div>
     </div>
     <h2>個人情報の管理</h2>
+    <div class="content">
+      <div class="small-content">
+        <p>
+          当社は、訪問者からご提供いただいた情報の管理について、以下を徹底します。
+        </p>
+      </div>
+      <h3>情報の正確性の確保</h3>
+      <div class="small-content">
+        <p>
+          訪問者からご提供いただいた情報については、常に正確かつ最新の情報となるよう努めます。
+        </p>
+      </div>
+      <h3>安全管理措置</h3>
+      <div class="small-content">
+        <p>
+          当社ホームページは、個人情報の漏えいや滅失又は棄損を防止するために、適切なセキリュティ対策を実施して個人情報を保護します。
+        </p>
+      </div>
+      <h3>個人情報の廃棄</h3>
+      <div class="small-content">
+        <p>
+          個人情報が不要となった場合には、すみやかに廃棄します。
+        </p>
+      </div>
+      <h3>個人情報の開示、訂正、追加、削除、利用停止</h3>
+      <div class="small-content">
+        <p>
+          訪問者ご本人からの個人情報の開示、訂正、追加、削除、利用停止のご希望の場合には、ご本人であることを確認させていただいた上、速やかに対応させていただきます。<br>
+          <nuxt-link to="/contact">お問い合わせフォーム</nuxt-link>よりお問い合わせください。
+        </p>
+      </div>
+      <h3>個人情報の第三者への提供について</h3>
+      <div class="small-content">
+        <p>
+          当サイトでは、個人情報は適切に管理し、以下に該当する場合を除いて第三者に開示することはありません。
+        </p>
+        <ul>
+          <li>本人のご了解がある場合</li>
+          <li>法令等への協力のため、開示が必要となる場合</li>
+        </ul>
+      </div>
+    </div>
     <h2>未成年の個人情報について</h2>
+    <div class="content">
+      <p>
+        未成年者が<nuxt-link to="/contact">お問い合わせフォーム</nuxt-link>から問い合わせをされたりする場合は必ず親権者の同意を得るものとし、お問い合わせをされた時点で、当プライバシーポリシーに対して親権者の同意があるものとみなします。
+      </p>
+    </div>
     <h2>アクセス解析ツールについて</h2>
+    <div class="content">
+      <div class="small-content">
+        <p>
+          当社ホームページでは、Googleによるアクセス解析ツール「Googleアナリティクス」を利用しています。<br>
+          このGoogleアナリティクスはアクセス情報の収集のためにCookieを使用しています。このアクセス情報は匿名で収集されており、個人を特定するものではありません。<br>
+          GoogleアナリティクスのCookieは、26ヶ月間保持されます（個々のGoogle Analytics の設定によります）。<br>
+          この機能はCookieを無効にすることで収集を拒否することが出来ますので、お使いのブラウザの設定をご確認ください。
+        </p>
+      </div>
+      <div class="small-content">
+        <p>
+          Googleアナリティクスの利用規約に関して確認したい場合は、Google Analyticsのサイトを、Google社のプライバシーポリシーに関する説明については同社のサイトをご覧ください。
+        </p>
+      </div>
+      <div class="small-content">
+        <div>
+          <a href="https://www.google.com/analytics/terms/jp.html">Google Analyticsの利用規約</a>（外部リンク）
+        </div>
+        <div>
+          <a href="https://policies.google.com/privacy?hl=ja">Googleのプライバシーポリシー</a>（外部リンク）
+        </div>
+      </div>
+    </div>
     <h2>プライバシーポリシーの変更について</h2>
     <div class="content">
       <p>
